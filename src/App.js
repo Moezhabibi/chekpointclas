305adb378bc6478f5a966148642fb308b0636485
Moezhabibi <moez.habibi123@gmail.com>
2022-11-07 09:01:39 +0100

    chekpointclass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      person : [
+        FullName , bio , imgSrc , profession
+
+      ],
+      show : false,
+    }
+  }
+
+  handleShow=()=> this.setState({...this.state,show : !this.state.show})
+
+
+      render(){
+        return(
+          <div>
+            
+            {
+              this.state.person.map(el=> 
+              <div>
+                <h2>{ el. FullName}</h2>
+                <h2>{el. bio}</h2>
+                <h2>{el. imgSrc}</h2>
+                <h2>{el. profession}</h2>
+                <button onClick={this.handleShow}>{this.state.show ? 'Hide' : 'Show'}</button>
+                {
+                  this.state.show && < handleShow = {this.state.person}> 
+                }
+              </div>)
+            }
+            </div>
+           
+          
+        
+          
+        
+      
+
+
 
 export default App;
